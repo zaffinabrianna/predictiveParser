8e93c4263d8a43dd5123c800b9f2c275a80b6b73
zaffinabrianna <zaffinabrianna@yahoo.com>
2025-04-14 21:52:25 -0700

    Added code to output the parser

--- parser.py
+++ parser.py
@@ -2,15 +2,16 @@
 # CPSC 323
 # Professor Venkatesh
 # 21 April 2025
-    # Creating Predictive Parsing Table:
+
+# Creating Predictive Parsing Table:
 parseTable = {
     'E':{
         'a':['T', "E'"],
         '(':['T', "E'"]
         },
     "E'":{
-        '+':['T', '+', "E'"],
-        '-':['T', '-', "E'"],
+        '+':['+', 'T', "E'"],
+        '-':['-', 'T', "E'"],
         ')':['ε'],
         '$':['ε']
         },
@@ -19,8 +20,8 @@ parseTable = {
         'a':['F', "T'"]
         },
     "T'":{
-        '*':['F', '*', "T'"],
-        '/':['F', '/', "T'"],
+        '*':['*', 'F', "T'"],
+        '/':['/', 'F', "T'"],
         '+':['ε'],
         '-':['ε'],
         '(':['ε'],
@@ -28,16 +29,68 @@ parseTable = {
         },
     'F':{
         '(':['(', 'E', ')'],
-        'a':['F']
+        'a':['a']
         }
 }
 
 states = ['a', '+', '-', '*', '/', '(', ')', '$']
+terms = ['E', 'T', "E'", "T'", 'F', 'ε']
+
+def parse(inputs):
+    inputs = inputs.replace(" ", "").strip()
+    
+    for char in inputs:
+        if char not in states and char not in terms:
+            print("String is Invalid.\n")
+            return
+        
+    stack = ['$', 'E']
+    index = 0
+
+    print(f"{'Stack':<30} {'Input':<30} Action")
+    while len(stack) > 0:
+        top = stack[-1]
+        currentInput = inputs[index]
+
+        print(f"{''.join(stack):<30} {inputs[index:]:<30}", end=' ')
+
+        if top == currentInput:
+            print(f"Match '{top}'")
+            stack.pop()
+            index += 1
+        elif top in states:
+            print(f"Error: Unexpected Terminal '{top}'")
+            return
+        elif top in parseTable and currentInput in parseTable[top]:
+            rule = parseTable[top][currentInput]
+
+            print(f"Apply {top} -> {' '.join(rule)}")
+
+            stack.pop()
+
+            if rule[0] != 'ε':
+                for symbol in reversed(rule):
+                    stack.append(symbol)
+        else:
+                print(f"Error: No Rule For {top} with input {currentInput}")
+                print(f"Output: String is Invalid.\n")
+                return
+            
+    if index == len(inputs):
+        print("String is Valid\n")
+    else:
+        print("String is Invalid\n")
 
 def main():
-    print("bruh")
-    
+    test = [
+        "(a +a)*a$",
+        "a*(a/a) $",
+        "a(a+a) $:"
+    ]
 
+    for i, s in enumerate(test, 1):
+        print(f"\nTest Case {i}: {s} ---")
+        parse(s)
 
 if __name__ == "__main__":
     main()
